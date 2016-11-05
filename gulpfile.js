b5aefe22b26089106deafd1b5f6301d2297b8ac9
monkeysbrain <monkeysbrain@yandex.ru>
2016-11-05 18:53:33 +0500

    set gulp, create 320

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var stylus = require('gulp-stylus');
+var rigger = require('gulp-rigger');
 
-gulp.task('sass', function(){
-    return gulp.src('app/scss/styles.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
+gulp.task('stylus', function(){
+    return gulp.src('app/stylus/style.stylus')
+        .pipe(stylus({
+		      'include css': true
+		}))
+        .pipe(gulp.dest('app/css/'))
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-})
+    gulp.watch('app/stylus/**/*.stylus', ['stylus']);
+});
+
+gulp.task('rigger', function () {
+    gulp.src('app/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('build/'));
+});
